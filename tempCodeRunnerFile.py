99c794f10f4c34b8e2900d1f2b29f43ccd7f3e7b
08hakr <hrishi20hak@gmail.com>
2024-03-23 20:47:45 +0530

    all files are added

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+    # response = requests.post(url, json=payload, headers=headers)
+
+    # json_data = json.loads(response.text)
+    # # print(result['microsoft']['result'])
+
+    # # Parse the JSON data
+
+    # # Extract the summary
+    # summary = json_data["openai"]["result"]
+    # # print(json_data)
+    # print(summary)
+    # print(rawdocs)

da428899cf8635025131f9546927c7ced35db1d2
Hrushikesh Anil Kshirsagar <67857453+08hakr@users.noreply.github.com>
2024-04-05 07:33:43 +0000

    fianl Changes

--- text_summary.py
+++ text_summary.py
@@ -1,28 +1,25 @@
-import spacy
-import json #a
-import requests #a
-from spacy.lang .en.stop_words import STOP_WORDS
-from string import punctuation
-from data import dataT #a
-from heapq import nlargest
+import spacy    #Natural language library 
+import json #a #for Working with JSON data
+import requests #a  #for sending HTTP requests
+import gensim #a #for text summarization
+from spacy.lang .en.stop_words import STOP_WORDS #for removing stopwords
+from string import punctuation  #for removing punctuations
+from data import dataT #a 
+from heapq import nlargest 
 
 
-# text = """The Asgardian Loki encounters the Other, the leader of an extraterrestrial race known as the Chitauri. In exchange for retrieving the Tesseract,[c] a powerful energy source of unknown potential, the Other promises Loki an army with which he can subjugate Earth. Nick Fury, director of the espionage agency S.H.I.E.L.D., arrives at a remote research facility, where physicist Dr. Erik Selvig is leading a team experimenting on the Tesseract. The Tesseract suddenly activates and opens a wormhole, allowing Loki to reach Earth. Loki steals the Tesseract and uses his scepter to enslave Selvig and other agents, including Clint Barton, to aid him.
-
-# In response, Fury reactivates the "Avengers Initiative". Agent Natasha Romanoff heads to Kolkata to recruit Dr. Bruce Banner to trace the Tesseract through its gamma radiation emissions. Fury approaches Steve Rogers to retrieve the Tesseract, and Agent Phil Coulson visits Tony Stark to have him check Selvig's research. Loki is in Stuttgart, where Barton steals the iridium needed to stabilize the Tesseract's power, leading to a confrontation with Rogers, Stark, and Romanoff that ends with Loki's surrender. While Loki gets escorted to S.H.I.E.L.D., his adoptive brother Thor arrives and frees him, hoping to convince him to abandon his plan and return to Asgard. Stark and Rogers intervene and Loki is taken to S.H.I.E.L.D.'s flying aircraft carrier, the Helicarrier, where he is imprisoned.
-
-# The Avengers become divided over how to approach Loki and the revelation that S.H.I.E.L.D. plans to harness the Tesseract to develop powerful weapons as a deterrent against hostile extraterrestrials. As they argue, Loki's agents attack the Helicarrier, and the stress causes Banner to transform into the Hulk. Stark and Rogers work to restart the damaged engine, and Thor attempts to stop the Hulk's rampage. Romanoff knocks Barton unconscious, breaking Loki's mind control. Loki escapes after killing Coulson and Fury uses Coulson's death to motivate the Avengers into working as a team. Loki uses the Tesseract and a wormhole generator Selvig built to open a wormhole above Stark Tower to the Chitauri fleet in space, launching his invasion.
-
-# Rogers, Stark, Romanoff, Barton, Thor, and the Hulk rally in defense of New York City, and together the Avengers battle the Chitauri. The Hulk beats Loki into submission. Romanoff makes her way to the generator, where Selvig, freed from Loki's mind control, reveals that Loki's scepter can shut down the generator. Fury's superiors from the World Security Council attempt to end the invasion by launching a nuclear missile at Midtown Manhattan. Stark intercepts the missile and takes it through the wormhole toward the Chitauri fleet. The missile detonates, destroying the Chitauri mothership and disabling their forces on Earth. Stark's suit loses power and he goes into freefall, but the Hulk saves him, while Romanoff uses Loki's scepter to close the wormhole. In the aftermath, Thor returns with Loki and the Tesseract to Asgard, where Loki will face their justice.
-
-# In a mid-credits scene, the Other confers with his master[d] about the failed attack on Earth."""
-
+#The Main fuction to summarize the text
 def summarizer(rawdocs):
+        #data Preperation Starts Here
+        #Loads English language model in spacy
         stopwords = list (STOP_WORDS)
         #print(stopwords)
         nlp = spacy.load('en_core_web_sm')
         doc = nlp (rawdocs)
-        #print(doc)
+        #print(doc)        
+        
+        
+        #Word Tokenization: Tokenization is the process of breaking the text into smaller parts called tokens
         tokens = [token.text for token in doc]
         #print (tokens)
         print(dataT["size"]) #a
@@ -30,13 +27,16 @@ def summarizer(rawdocs):
         url=dataT["url"] #a
         headers = dataT["headers"] #a
         payload = {
-            "providers": "openai",
+            "providers": dataT["a"],
             "output_sentences": size,
             "language": "en",
             "text": rawdocs,
             "fallback_providers": ""
         } #a
-
+        #data Preperation Ends Here
+        
+        
+        #Calculating the Wrequency of words in the text: with removing StopWords and Punctuations
         word_freq ={}
         for word in doc:
             if word.text.lower() not in stopwords and word.text.lower() not in punctuation:
@@ -45,20 +45,24 @@ def summarizer(rawdocs):
                 else: 
                     word_freq[word.text] += 1
         #print(word_freq)
-
-
         max_freq =max(word_freq.values())
         #print (max_freq)
-
-
+        
+        
+        #Frequence Normalization: By deviding each frequency by maximum frequency
         for word in word_freq.keys():
             word_freq[word] = word_freq[word]/max_freq
         #print(word_freq)
-
-
+        #frequency of words ends here
+        
+        
+        #Sentence tokenization: Using spacy
         sent_tokens = [sent for sent in doc.sents]
         #print (sent_tokens)
-
+        #Sentence tokenization ends here
+        
+        
+        #Sentence Scoring: Calculating the score of each sentence by adding the frequency of the words that are present in the sentence
         sent_scores = {}
         for sent in sent_tokens:
             for word in sent :
@@ -68,10 +72,12 @@ def summarizer(rawdocs):
                     else:
                         sent_scores[sent] += word_freq[word.text]
         #print(sent_scores)
-
+        #Sentence Scoring ends here
+        
+        
+        #Summary Creation: Selecting 30% of the sentences having the highest scores.
         select_len = int (len (sent_tokens) * 0.3)
         #print(select_len)
-
         summary = nlargest(select_len,sent_scores,key=sent_scores.get)
         #print(summary)
         final_summary = [word.text for word in summary]
@@ -80,22 +86,19 @@ def summarizer(rawdocs):
         # print(summary)
         # print("Length of original text ",len(text.split(' ')))
         # print("Length of summary text ",len(summary.split(' ')))
+        
+        #gensim Working Starts: Using Gensim for Text Summarization for Abstractive Summarazation.
         response = requests.post(url, json=payload, headers=headers) #a
 
         json_data = json.loads(response.text) #a
-        # print(result['microsoft']['result']) #a
-
         # Parse the JSON data
-
         # Extract the summary
-        summary = json_data[dataT["a"]]["result"] #a
+        summary =     summary = json_data[dataT["a"]]["result"]
         # print(json_data) #a
         return summary,rawdocs,len(rawdocs.split(' ')),len(summary.split(' ')) #a
 
 
 # def summarizer(rawdocs):
-    
-    
 #     print(summary)
 #     print(rawdocs)
 #     return summary,rawdocs,len(rawdocs.split(' ')),len(summary.split(' '))
